b631a6c27b1f832e25951df5af3693fd169a54b1
GREEN <GREEN@G704-03>
2024-02-19 10:01:29 +0900

    userController 업데이트

---
+++ src/main/java/com/ex/springboot/controller/UserController.java
@@ -0,0 +1,90 @@
+package com.ex.springboot.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ex.springboot.dao.IuserDAO;
+import com.ex.springboot.dto.UserDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping
+public class UserController {
+
+	@Autowired
+	IuserDAO dao;
+	
+	@GetMapping("/user/join")
+	public String join() {
+		return "user/join";
+	}
+	
+	@PostMapping("/user/joinProcess")
+	public String joinProcess(UserDTO userDTO) {
+	
+		if(dao.joinDAO(userDTO)) {
+			System.out.println("회원가입 성공");
+			return "redirect:/";
+		} else{
+			System.out.println("회원가입 실패");
+			return "/user/join";
+		}
+	
+	}
+	
+	@GetMapping("/user/myInfoUpdate")
+	public String myInfo(HttpServletRequest request, Model model) {
+		List<UserDTO> userDTO = new ArrayList<>();
+		
+		HttpSession session = request.getSession();
+		int u_seq = (int) session.getAttribute("ss_u_seq");
+		
+		if(u_seq > 0) {
+			userDTO = dao.myInfoDAO(u_seq);
+			UserDTO userInfo = userDTO.get(0);
+			model.addAttribute("userInfo", userInfo);
+			return "/user/myInfoUpdate";
+		} else {
+			return "/user/login";
+		}
+	}
+	
+	@GetMapping("/user/login")
+	public String login() {
+		return "/user/login";
+	}
+	
+	@PostMapping("/user/loginProcess")
+	public String loginProcess(@RequestParam(value = "u_id") String u_id, @RequestParam(value = "u_pw") String u_pw, HttpServletRequest request) {
+		List<UserDTO> userDTO = new ArrayList<>();
+		
+		System.out.println("U_ID : "+u_id);
+		System.out.println("U_PW : "+u_pw);
+		
+		userDTO = dao.loginDAO(u_id, u_pw);
+		
+		UserDTO userInfo = userDTO.get(0);
+		
+		if(userDTO.size() > 0) {
+			HttpSession session = request.getSession();
+			
+			session.setAttribute("ss_u_seq", userInfo.getU_seq());
+			System.out.println("로그인 성공");
+			return "redirect:/";
+		}else {
+			System.out.println("로그인 실패");
+			return "/user/login";
+		}
+	}
+	
+}
