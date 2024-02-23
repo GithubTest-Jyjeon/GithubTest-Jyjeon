426d473c2dcf92675baa1d465889f0d1cd659a6f
GREEN <GREEN@G704-12>
2024-02-23 15:12:14 +0900

    고쳐줘 2

---
+++ src/main/resources/static/js/product_index.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+  // 수량 증가 및 감소 버튼 이벤트 처리
+  $("#plusBtn").click(function () {
+    var currentValue = parseInt($("#quantityInput").val());
+    $("#quantityInput").val(currentValue + 1);
+  });
+
+  $("#minusBtn").click(function () {
+    var currentValue = parseInt($("#quantityInput").val());
+    if (currentValue > 1) {
+      $("#quantityInput").val(currentValue - 1);
+    }
+  });
+
+  // 카드 클릭 이벤트
+  $(".card").click(function (e) {
+    // 이동을 원하는 조건을 여기에 명시
+    if (!$("#staticBackdrop").hasClass("show")) {
+      window.location.href = $(this).data("href");
+    }
+  });
+
+  // 버튼 클릭 이벤트 중지
+  $(".btn-primary, .btn-close, .btn-secondary").click(function (event) {
+    event.stopPropagation();
+  });
+
+  // "더보기" 버튼 로직
+  var initialCards = 4;
+  var cards = $(".card");
+  var totalCards = cards.length;
+
+  // 초기 카드 표시
+  cards.slice(0, initialCards).show();
+
+  $("#loadMore").click(function (e) {
+    e.preventDefault();
+    var visibleCards = $(".card:visible").length;
+    var next = visibleCards + 4; // 다음에 보여질 카드 수
+
+    cards.slice(visibleCards, next).show();
+
+    // 모든 카드가 표시된 경우 "더보기" 버튼 숨김
+    if (next >= totalCards) {
+      $(this).hide();
+    }
+  });
+
+  // 모든 카드가 처음부터 보이면 "더보기" 버튼 숨김
+  if (totalCards <= initialCards) {
+    $("#loadMore").hide();
+  }
+});
