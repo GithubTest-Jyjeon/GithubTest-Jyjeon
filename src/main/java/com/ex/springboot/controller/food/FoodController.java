adb80ce7010b472404cedea1b8d27a00fc2c6fa5
GREEN <GREEN@G704-12>
2024-03-04 16:28:05 +0900

    아아 아아 아아이

--- src/main/java/com/ex/springboot/controller/food/FoodController.java
+++ src/main/java/com/ex/springboot/controller/food/FoodController.java
@@ -1,5 +1,11 @@
 package com.ex.springboot.controller.food;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,8 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ex.springboot.dto.FoodDTO;
+import com.ex.springboot.dto.ProductDTO;
 import com.ex.springboot.dto.UserDTO;
 import com.ex.springboot.interfaces.IfoodDAO;
+import com.ex.springboot.interfaces.IproductDAO;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -19,6 +28,9 @@ public class FoodController {
 
 	@Autowired
 	IfoodDAO dao;
+	
+	@Autowired
+	IproductDAO daoproduct;
 	
 	@GetMapping("/food/list")
 	public String foodList(@RequestParam(value="page", defaultValue="1") int page, @RequestParam(value = "b_seq") int b_seq, Model model, HttpServletRequest request) {
@@ -56,13 +68,44 @@ public class FoodController {
 	@GetMapping("/food/view")
 	public String foodView(@RequestParam(value = "f_code") String f_code, Model model) {
 		
-		model.addAttribute("foodInfo", dao.foodView(f_code));
-		model.addAttribute("f_code", f_code);
-		
-		System.out.println("f_code : "+f_code);
-		System.out.println("foodInfo : ");
-		System.out.println(dao.foodView(f_code));
-		
-		return "/food/view";
+	    // 기존 코드
+	    model.addAttribute("foodInfo", dao.foodView(f_code));
+	    model.addAttribute("f_code", f_code);
+
+	    // Recipe List 로직
+	    ArrayList<String> recipeList = new ArrayList<>();
+	    FoodDTO foodDTO = dao.foodView(f_code);
+	    StringTokenizer token = new StringTokenizer(foodDTO.getF_recipe(), "|");
+	    while(token.hasMoreElements()) {
+	        recipeList.add((String) token.nextElement());
+	    }
+	    model.addAttribute("recipeList", recipeList);
+
+	    
+	    // f_code_arr 처리 로직
+	    ArrayList<Map> ingredientList = new ArrayList<>();
+	    
+	    StringTokenizer tokenFCode = new StringTokenizer(foodDTO.getF_code_arr(), "|");
+	    StringTokenizer tokenFVolume = new StringTokenizer(foodDTO.getF_volume_arr(), "|");
+	    while(tokenFCode.hasMoreElements()) {
+	    	Map<String, String> map = new HashMap<>();
+	    	ProductDTO pInfo = daoproduct.productInfo((String)tokenFCode.nextElement());
+	    	String fVolume = (String)tokenFVolume.nextElement();
+	    	String pSeq = pInfo.getP_seq()+"";
+	    	map.put("pSeq", pSeq);
+	    	map.put("pName", pInfo.getP_name());
+	    	map.put("fVolume", fVolume);
+	    	ingredientList.add(map);
+	    }
+	    
+	    System.out.println(ingredientList);
+	    model.addAttribute("ingredientList", ingredientList);
+	    
+	    
+	    
+	    
+
+	    return "/food/view";
 	}
+
 }
